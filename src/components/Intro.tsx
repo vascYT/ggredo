15817d9c21ffbe0936a45eb5f5ee2525681bb27d
vasc <nicovanaschaufler@gmail.com>
2021-10-19 00:07:07 +0200

    add subheading

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,13 +11,20 @@ export default function Intro() {
 						<ModelWithNoSSR />
 					</div>
 				</div>
-				<div className="w-full md:w-[50%]">
-					<div className="flex items-center justify-center">
-						<h1 className="text-5xl md:text-[5vw] leading-none font-bold font-intro mt-[5vh] md:mt-[20vh]">
-							<span>Goethe</span>
-							<br />
-							<span className="ml-[7vw]">Gymnasium</span>
-						</h1>
+				<div className="w-full md:w-[50%] font-intro">
+					<div className="flex flex-col">
+						<div className="w-full ml-[2vw]">
+							<h1 className="text-5xl md:text-[5vw] leading-none font-bold mt-[5vh] md:mt-[20vh]">
+								<span>Goethe</span>
+								<br />
+								<span className="ml-[7vw]">Gymnasium</span>
+							</h1>
+						</div>
+						<div className="w-full">
+							<p className="text-2xl md:text-[1.75vw] opacity-60 text-right mr-[8vw]">
+								FÜR DEINE ZUKUNFT
+							</p>
+						</div>
 					</div>
 				</div>
 				<div className="w-[50%] hidden md:block">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({Component, pageProps}: AppProps) {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
