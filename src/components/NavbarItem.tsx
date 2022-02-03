0f66c72eadcac1a01510782cf12f261a21abd8ef
vascYT <nicovanaschaufler@gmail.com>
2022-02-03 18:08:11 +0100

    add: arrow animation

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,6 +1,7 @@
 import {Popover, Transition} from "@headlessui/react";
 import {PropsWithChildren, useState} from "react";
 import {RiArrowDropDownLine} from "react-icons/ri";
+import Image from "next/image";
 
 interface props {
 	name: string;
@@ -9,34 +10,39 @@ interface props {
 export default function Dropdown(props: PropsWithChildren<props>) {
 	return (
 		<Popover>
-			<Popover.Button className="font-text inline-flex items-center">
-				{props.name} <RiArrowDropDownLine className="h-6 w-6" />
-			</Popover.Button>
+			{({open}) => (
+				<>
+					<Popover.Button className="font-text inline-flex items-center">
+						{props.name}{" "}
+						<RiArrowDropDownLine className={`transition-all h-6 w-6 ${open ? "rotate-180" : ""}`} />
+					</Popover.Button>
 
-			<Transition
-				enter="transition ease-out duration-200"
-				enterFrom="opacity-0 translate-y-1"
-				enterTo="opacity-100 translate-y-0"
-				leave="transition ease-in duration-150"
-				leaveFrom="opacity-100 translate-y-0"
-				leaveTo="opacity-0 translate-y-1"
-			>
-				<Popover.Panel className="absolute z-10 mt-3 transform -translate-x-1/2">
-					<div className="grid grid-cols-2 w-[350px] h-[250px] bg-white p-2 gap-x-2 gap-y-2 rounded-lg shadow-lg">
-						{props.items.map(item => (
-							<div
-								className={
-									"flex flex-col items-center justify-center col-span-1 bg-no-repeat bg-cover bg-center h-full rounded-lg"
-								}
-								style={{backgroundImage: `url("${item.imageUrl}")`}}
-								key={item.name}
-							>
-								<a href={item.href}>{item.name}</a>
+					<Transition
+						enter="transition ease-out duration-200"
+						enterFrom="opacity-0 translate-y-1"
+						enterTo="opacity-100 translate-y-0"
+						leave="transition ease-in duration-150"
+						leaveFrom="opacity-100 translate-y-0"
+						leaveTo="opacity-0 translate-y-1"
+					>
+						<Popover.Panel className="absolute z-10 mt-3 transform -translate-x-1/2">
+							<div className="grid grid-cols-2 w-[350px] h-[250px] bg-white p-2 gap-x-2 gap-y-2 rounded-lg shadow-lg">
+								{props.items.map(item => (
+									<div
+										className={
+											"flex flex-col items-center justify-center col-span-1 bg-no-repeat bg-cover bg-center h-full rounded-lg"
+										}
+										style={{backgroundImage: `url("${item.imageUrl}")`}}
+										key={item.name}
+									>
+										<a href={item.href}>{item.name}</a>
+									</div>
+								))}
 							</div>
-						))}
-					</div>
-				</Popover.Panel>
-			</Transition>
+						</Popover.Panel>
+					</Transition>
+				</>
+			)}
 		</Popover>
 	);
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,17 +1,25 @@
+import Head from "next/head";
 import {getAllPostIds, getPost} from "../../../lib/posts";
 
 export default function Post({post}: any) {
 	return (
-		<div className="bg-white w-full">
-			<div className="bg-wave2 bg-cover bg-no-repeat w-full h-[250px] bg-center">
-				<div className="flex flex-col justify-center h-[80%] pl-[25vw] pr-[25vw]">
-					{/* TODO: Center title on mobile */}
-					<p className="text-base text-gray-200">{post.head.date}</p>
-					<h1 className="text-white font-title text-7xl font-bold uppercase">{post.head.title}</h1>
+		<>
+			<Head>
+				<title>{post.head.title} - Goethe Gymnasium</title>
+			</Head>
+			<div className="bg-white w-full">
+				<div className="bg-wave2 bg-cover bg-no-repeat w-full h-[250px] bg-center">
+					<div className="flex flex-col justify-center h-[80%] pl-[25vw] pr-[25vw]">
+						{/* TODO: Center title on mobile */}
+						<p className="text-base text-gray-200">{post.head.date}</p>
+						<h1 className="text-white font-title text-7xl font-bold uppercase">
+							{post.head.title}
+						</h1>
+					</div>
 				</div>
+				<div className="pl-[15%] pr-[15%]" dangerouslySetInnerHTML={{__html: post.body}} />
 			</div>
-			<div className="pl-[15%] pr-[15%]" dangerouslySetInnerHTML={{__html: post.body}} />
-		</div>
+		</>
 	);
 }
 
